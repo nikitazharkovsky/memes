ab79069d712a12e820de708cf82953b21e0cebc8
Nikita Zharkovsky <nikita.zharkovsky@gmail.com>
2020-11-24 11:51:22 +0300

    Вызов запроса на авторизацию. Сохранение результатов в shared preferences

--- app/src/main/java/com/zharkovsky/memes/activities/LoginActivity.kt
+++ app/src/main/java/com/zharkovsky/memes/activities/LoginActivity.kt
@@ -1,13 +1,29 @@
 package com.zharkovsky.memes.activities
 
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.text.InputType
 import android.view.View
 import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
-import com.zharkovsky.memes.R
-import com.zharkovsky.memes.utils.Constants
+import com.google.android.material.snackbar.Snackbar
+import com.zharkovsky.memes.services.NetworkService
 import com.zharkovsky.memes.utils.ProgressButtonHelper
+import com.zharkovsky.memes.R
+import com.zharkovsky.memes.models.LoginUserRequestDto
+import com.zharkovsky.memes.models.AuthInfoDto
+import com.zharkovsky.memes.utils.Constants
+import com.zharkovsky.memes.utils.Constants.APP_REFERENCES
+import com.zharkovsky.memes.utils.Constants.FIRST_NAME_FIELD
+import com.zharkovsky.memes.utils.Constants.ID_FIELD
+import com.zharkovsky.memes.utils.Constants.LAST_NAME_FIELD
+import com.zharkovsky.memes.utils.Constants.TOKEN_FIELD
+import com.zharkovsky.memes.utils.Constants.USERNAME_FIELD
+import com.zharkovsky.memes.utils.Constants.USER_DESCRIPTION_FIELD
+import retrofit2.Call
+import retrofit2.Callback
 import studio.carbonylgroup.textfieldboxes.ExtendedEditText
 import studio.carbonylgroup.textfieldboxes.TextFieldBoxes
 import java.util.*
@@ -19,6 +35,8 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        var authLayout = findViewById<LinearLayout>(R.id.auth_layout)
 
         var loginTextFieldBoxes = findViewById<TextFieldBoxes>(R.id.text_field_boxes_login)
         var loginExtendedEditText = findViewById<ExtendedEditText>(R.id.extended_edit_text_login)
@@ -40,7 +58,11 @@ class LoginActivity : AppCompatActivity() {
             val inputIsValid = validateInput(loginTextFieldBoxes, loginExtendedEditText, true) and
                     validateInput(passwordTextFieldBoxes, passwordExtendedEditText, !loginTextFieldBoxes.isOnError)
             if (inputIsValid) {
-                onLoginClick(progressButtonHelper)
+                var request = LoginUserRequestDto(
+                        loginExtendedEditText.text.toString(),
+                        passwordExtendedEditText.text.toString()
+                )
+                onLoginClick(authLayout, progressButtonHelper, request)
             }
         }
     }
@@ -89,12 +111,46 @@ class LoginActivity : AppCompatActivity() {
         return true
     }
 
-    private fun onLoginClick(progressButtonHelper: ProgressButtonHelper) {
+    private fun onLoginClick(root: View, progressButtonHelper: ProgressButtonHelper, request: LoginUserRequestDto) {
+        var rootActivity = this;
         progressButtonHelper.buttonActivated()
 
         Timer().schedule(timerTask {
+            NetworkService.getInstance()
+                    .jsonApi
+                    .login(request)
+                    .enqueue(object : Callback<AuthInfoDto> {
+                        override fun onResponse(call: Call<AuthInfoDto>, response: retrofit2.Response<AuthInfoDto>) {
+                            saveResponseToSharedPref(rootActivity, response.body()!!)
+//                            var nextActivityIntent = Intent(rootActivity, MainActivity::class.java)
+//                            startActivity(nextActivityIntent)
+//                            finish()
+                        }
+
+                        override fun onFailure(call: Call<AuthInfoDto>, t: Throwable) {
+                            val snackbar = Snackbar.make(
+                                    root,
+                                    getString(R.string.auth_error),
+                                    Snackbar.LENGTH_LONG
+                            )
+                            snackbar.view.setBackgroundResource(R.color.errorBackground)
+                            snackbar.show()
+                        }
+                    })
             runOnUiThread(progressButtonHelper::buttonFinished)
         }, Constants.PROGRESS_BUTTON_DELAY)
     }
 
+    private fun saveResponseToSharedPref(activity: Activity, response: AuthInfoDto) {
+        val sharedPref = activity.getSharedPreferences(APP_REFERENCES, Context.MODE_PRIVATE) ?: return
+        with (sharedPref.edit()) {
+            putString(TOKEN_FIELD, response.accessToken)
+            putInt(ID_FIELD, response.userInfo!!.id)
+            putString(USERNAME_FIELD, response.userInfo!!.username)
+            putString(FIRST_NAME_FIELD, response.userInfo!!.firstName)
+            putString(LAST_NAME_FIELD, response.userInfo!!.lastName)
+            putString(USER_DESCRIPTION_FIELD, response.userInfo!!.userDescription)
+            commit()
+        }
+    }
 }
--- app/src/main/java/com/zharkovsky/memes/utils/Constants.kt
+++ app/src/main/java/com/zharkovsky/memes/utils/Constants.kt
@@ -3,6 +3,15 @@ package com.zharkovsky.memes.utils
 object Constants {
     const val SPLASH_DELAY = 300L
     const val PROGRESS_BUTTON_DELAY = 3000L
+
     const val BASE_URL = "https://virtserver.swaggerhub.com"
     const val AUTH_URL = "/AndroidSchool/SurfAndroidSchool/1.0.0/auth/login"
+
+    const val APP_REFERENCES = "common_settings"
+    const val TOKEN_FIELD = "token"
+    const val ID_FIELD = "id"
+    const val USERNAME_FIELD = "username"
+    const val FIRST_NAME_FIELD = "firstName"
+    const val LAST_NAME_FIELD = "lastName"
+    const val USER_DESCRIPTION_FIELD = "userDescription"
 }
